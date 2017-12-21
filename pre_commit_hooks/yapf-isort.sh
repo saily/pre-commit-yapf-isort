2edc13cbff340a81e6d878eb9e8e8938f088b823
Philipp Allgeuer <philipp.allgeuer@zumtobelgroup.com>
2017-12-21 16:34:08 +0100

    impl. virtualenv support

--- pre_commit_hooks/yapf-isort.sh
+++ pre_commit_hooks/yapf-isort.sh
@@ -3,8 +3,26 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+
+
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
+
+# check if classic virtualenv is used
+echo 'Check virtualenv'
+if [ -f bin/activate ]; then
+  source bin/activate
+fi
+
+# check if virtualenvwrapper is used
+echo 'Check virtualenvwrapper'
+if [ ! -z "${WORKON_HOME}" ] && [ -f "${WORKON_HOME}/${PWD##*/}" ]; then
+  if ! which workon &>/dev/null; then
+    >&2 echo 'workon command not found'
+    exit 1
+  if
+  workon "${PWD##*/}"
+fi
 
 echo 'Begin yapf'
 if ! which yapf &>/dev/null; then
